cbf33c8b6cf423d6deb39c82e78047a975b96ad4
Freddy Fallon <freddyfallon@gmail.com>
2018-04-26 14:40:07 +0100

    Add random comsumers of queue

--- consumer.js
+++ consumer.js
@@ -1,9 +1,16 @@
 const amqplib = require('amqplib');
 
-const consumeQueue = async (ch, queueName) => {
+
+const consumeQueue = (ch, queueName) => {
   try {
     ch.assertQueue(queueName, {durable: false});
-    ch.consume(queueName, message => {console.log(`Received ${message.toString()}`), {noAck: true}})
+    ch.prefetch(Math.floor(Math.random() * 30 + 1));
+    ch.consume(queueName, (message) => {
+      console.log(`Received ${message.content.toString()}`)
+      setTimeout(() => {
+        ch.ack(message);
+      }, Math.floor(Math.random() * 2000 + 1) + 1000)
+    })
   } catch (err) {
     console.error('🔴', err)
   }
